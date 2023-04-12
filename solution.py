f5be1566b0bb5a57d216554c383f06d21e6a3564
n0thingseri0us <108615095+n0thingseri0us@users.noreply.github.com>
2023-04-13 02:09:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
-import math
 
 chat_id = 371649437 # Ваш chat ID, не меняйте название переменной
 
-def solution(control: np.array, test: np.array) -> bool:
-    
-     # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    # Ваш ответ, True или False
-    alpha = 0.01
+def solution(x: np.array, y: np.array) -> bool:
 
-    res = (stats.ttest_ind(control, test, alternative = "two-sided").pvalue < alpha)
-    return res
+    alpha = 0.01
+    
+    p_value = stats.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=5000,
+                 alternative='greater').pvalue 
+    return p_value < alpha
+    return pvalue <= alpha
