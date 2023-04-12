4eb36502b418b7b0042626630aa61131d38009ad
n0thingseri0us <108615095+n0thingseri0us@users.noreply.github.com>
2023-04-12 22:53:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
+import math
 
+chat_id = 371649437 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(control: np.array, test: np.array) -> bool:
+    
+     # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Ваш ответ, True или False
+    alpha = 0.01
+
+    res = (stats.ttest_ind(control, test, alternative = "two-sided").pvalue < alpha)
+    return res
